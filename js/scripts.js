d6e067a8c7a2d91c4041a8f03f8e8113f54d5600
Divyansh Tiwari <tiwaridivyansh@gmail.com>
2021-05-16 11:41:46 +0530

    NPM Scripts Part1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  
+    $("#reservebutton").click(function(){
+            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                $("#mycarousel").carousel('pause');
+                $("#carouselButton").children("span").removeClass('fa-pause');
+                $("#carouselButton").children("span").addClass('fa-play');
+            }
+            else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                $("#mycarousel").carousel('cycle');
+                $("#carouselButton").children("span").removeClass('fa-play');
+                $("#carouselButton").children("span").addClass('fa-pause');                    
+            }
+        });
+        $("#reservebutton").click(function(){
+            $("#reserveModal").modal('toggle');
+            });
+            $("#login").click(function(){
+            $("#loginModal").modal('toggle');
+            });
+});
